5f1ae5c03b160651415046fac19216a18c702788
DuduziGit <petitemberteeduardo@gmail.com>
2025-06-02 17:00:24 -0300

    codigo atualizado

---
+++ main.js
@@ -0,0 +1,53 @@
+const especialidades = document.querySelector('#especialidades')
+
+especialidades.addEventListener('click', () =>{
+    window.scrollTo({top: 790, left:0, behavior: 'smooth'})
+})
+
+
+const portifolio = document.querySelector('#portifolio')
+
+portifolio.addEventListener('click', () =>{
+    window.scrollTo({top: 1950, left:0, behavior: 'smooth'})
+})
+
+
+
+
+const especialidades_mobile = document.querySelector('#especialidades_mobile')
+
+especialidades_mobile.addEventListener('click', () =>{
+    window.scrollTo({top: 1400, left:0, behavior: 'smooth'})
+})
+
+
+
+const portifolio_mobile = document.querySelector('#portifolio_mobile')
+
+portifolio_mobile.addEventListener('click', () =>{
+    window.scrollTo({top: 3800, left:0, behavior: 'smooth'})
+})
+
+
+
+
+
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay')
+
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
---
+++ reveal.js
@@ -0,0 +1,92 @@
+window.revelar = ScrollReveal({reset:true})
+
+//parte sobre
+revelar.reveal('.efeito-img-sobre',{
+
+    duration: 1500,
+    distance: '80px'
+})
+
+revelar.reveal('.efeito-txt-sobre',{
+
+    duration: 1500,
+    distance: '80px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-btn-social',{
+
+    duration: 1500,
+    distance: '80px',
+    delay: 500
+})
+
+
+
+//os titulos
+revelar.reveal('.titulo',{
+
+    duration: 2000,
+    distance: '60px'
+})
+
+
+//as especialidades
+revelar.reveal('.efeito-espec1',{
+
+    duration: 1200,
+    distance: '60px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-espec2',{
+
+    duration: 1200,
+    distance: '60px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-espec3',{
+
+    duration: 1200,
+    distance: '60px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-espec4',{
+
+    duration: 1200,
+    distance: '60px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-espec5',{
+
+    duration: 1200,
+    distance: '60px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-espec6',{
+
+    duration: 1200,
+    distance: '60px',
+    delay: 500,
+    origin: 'left'
+})
+
+
+
+//meus projetos
+revelar.reveal('.img-port',{
+
+    duration: 1500,
+    distance: '140px',
+    delay: 500,
+    origin: 'right'
+})
